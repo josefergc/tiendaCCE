d3e81139b08b09d958a1dfd7bca77f5fc6026e00
josefergc <63210807+josefergc@users.noreply.github.com>
2022-04-21 17:56:17 -0500

    Update getCart.py
    version alpha de la prueba tecnica CCE

--- getCart.py
+++ getCart.py
@@ -1,24 +1,26 @@
 from pymongo import MongoClient
 import json
 
+#Crea conexion mongo
 myclient = MongoClient("mongodb+srv://"+varMongoUser+":"+varMongoPassword+"@cluster0.xbunp.mongodb.net")
 mydb = myclient["prueba"]
 mycol = mydb["carrito"]
 
-#Valida si no envian ningun parametro
+#Valida si no envian ningun parametro y devuelve error
 if not event["queryStringParameters"]:
     return {
         'statusCode': 406,
         'body': json.dumps('El campo user_id es necesario para continuar con la accion')
     }   
 
-#Valida si envia el campo usuario
+#Valida si envia el campo usuario y devuelve error
 if not 'user_id' in event["queryStringParameters"]:
     return {
             'statusCode': 406,
             'body': json.dumps('El campo user_id es necesario para continuar con la accion')
     } 
 
+#obtiene el usuario del parametro y genera el filtro de busqueda
 user = int(event["queryStringParameters"]['user_id'])
 myquery = {"user_id":user}
 
