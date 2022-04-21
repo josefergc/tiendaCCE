4cbb37390b4af57e64b26c91aebc40f440819b15
josefergc <63210807+josefergc@users.noreply.github.com>
2022-04-21 17:53:44 -0500

    Update deleteCart.py

--- deleteCart.py
+++ deleteCart.py
@@ -1,12 +1,14 @@
 from pymongo import MongoClient
 import json
 
+#Crea conexion mongo
 myclient = MongoClient("mongodb+srv://"+varMongoUser+":"+varMongoPassword+"@cluster0.xbunp.mongodb.net")
 mydb = myclient["prueba"]
 mycol = mydb["carrito"]
 
-    
+#captura el payload    
 data = json.loads(event["body"])
+
 #Valida el campo user_id
 if not 'user_id' in data:
     return {
@@ -20,13 +22,14 @@ if not 'product_id' in data and not 'delete_all' in data:
     'body': json.dumps('Debe especificar que producto desea eliminar')
 }
 
-#inicializa la variable
+#inicializa la variable para filtar el borrado
 myquery = {}
-#arma el query
+
+#arma el query si es un solo producto
 if 'product_id' in data:
     myquery = {"user_id":data["user_id"], "product_id":data["product_id"]}
 
-#La opción delete all tiene prioridad y cambia el query
+#La opción "delete all" tiene prioridad y cambia el filtro
 if 'delete_all' in data:
     if data['delete_all']=='true':
         myquery = {"user_id":data["user_id"]}        
