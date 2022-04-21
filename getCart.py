51210bf9d4f9bde7a93673bb59e455a93bc680d7
josefergc <63210807+josefergc@users.noreply.github.com>
2022-04-21 18:00:59 -0500

    version alpha de la prueba tecnica CCE

--- getCart.py
+++ getCart.py
@@ -27,11 +27,13 @@ myquery = {"user_id":user}
 #variable para crear el resultado (listado de productos en el carrito)
 dictItems = {}
 item = 0
+
 #recorre cada producto y lo deja en el formato de salida
 for product in mycol.find(myquery):
     item+=1
     dictItems["item_"+str(item)] = {"producto":product["nombre"],"producto":product["nombre"],"cantidad":product["cantidad"], "precio":product["precio"]}
     
+#Devuelve resultado    
 return {
     'statusCode': 200,
     'body': json.dumps({'cart':dictItems})
